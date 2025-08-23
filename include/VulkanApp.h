d520cc4031ba0b57c654dbd0a51f13e7e1bff460
C106 <pach3dm@gmail.com>
2025-08-23 21:39:23 +0800

    Init

--- include/VulkanApp.h
+++ include/VulkanApp.h
@@ -30,22 +30,3 @@ struct VulkanApp {
     void cleanup();
 };
 
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
-
-inline void check_vk(VkResult result, const char* msg = "") {
-    if (result != VK_SUCCESS) {
-        std::ostringstream oss;
-        oss << "Vulkan error " << result;
-        if (msg && msg[0] != '\0') {
-            oss << " at " << msg;
-        }
-
-        // 打印到标准错误
-        std::cerr << oss.str() << std::endl;
-
-        // 然后抛出异常
-        throw std::runtime_error(oss.str());
-    }
-}
