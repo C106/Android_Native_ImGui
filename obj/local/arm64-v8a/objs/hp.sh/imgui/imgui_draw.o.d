a153b6ba61e08ec7acea9dd42128048f748817a8
C106 <pach3dm@gmail.com>
2025-09-12 23:12:33 +0800

    Ignore build artifacts

--- obj/local/arm64-v8a/objs/hp.sh/imgui/imgui_draw.o.d
+++
@@ -1,8 +0,0 @@
-./obj/local/arm64-v8a/objs/hp.sh/imgui/imgui_draw.o: imgui/imgui_draw.cpp \
-  imgui/imgui.h imgui/imconfig.h imgui/imgui_internal.h \
-  imgui/misc/freetype/imgui_freetype.h imgui/imstb_rectpack.h
-imgui/imgui.h:
-imgui/imconfig.h:
-imgui/imgui_internal.h:
-imgui/misc/freetype/imgui_freetype.h:
-imgui/imstb_rectpack.h:
